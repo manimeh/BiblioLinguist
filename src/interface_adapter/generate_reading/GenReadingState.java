3b4f5fe2f97e3da364a15904edd397c7a1c0f41b
mark <mark.henein9616@gmail.com>
2023-11-10 14:49:30 -0500

    Implemented the boilerplate for the use case states

--- src/interface_adapter/generate_reading/GenReadingState.java
+++ src/interface_adapter/generate_reading/GenReadingState.java
@@ -1,4 +1,45 @@
 package interface_adapter.generate_reading;
 
+import entity.DifficultyLevel;
+import entity.language.Language;
+import entity.reading.ReadingType;
+
 public class GenReadingState {
+    private Language language;
+    private ReadingType reading;
+    private DifficultyLevel difficultyLevel;
+
+    public GenReadingState(Language language, ReadingType reading, DifficultyLevel difficultyLevel) {
+        this.language = language;
+        this.reading = reading;
+        this.difficultyLevel = difficultyLevel;
+    }
+
+    public GenReadingState() {}
+
+    // Getting functions
+    public Language getLanguage() {
+        return this.language;
+    }
+
+    public ReadingType getReading() {
+        return this.reading;
+    }
+
+    public DifficultyLevel getDifficultyLevel() {
+        return this.difficultyLevel;
+    }
+
+    // Setting functions
+    public void setLanguage(Language language) {
+        this.language = language;
+    }
+
+    public void setReading(ReadingType reading) {
+        this.reading = reading;
+    }
+
+    public void setDifficultyLevel(DifficultyLevel difficultyLevel) {
+        this.difficultyLevel = difficultyLevel;
+    }
 }
--- src/interface_adapter/take_quiz/TakeQuizState.java
+++ src/interface_adapter/take_quiz/TakeQuizState.java
@@ -1,4 +1,34 @@
 package interface_adapter.take_quiz;
 
+import entity.quiz.MCQuiz;
+import entity.user.User;
+
 public class TakeQuizState {
+    private User user;
+    private MCQuiz quiz;
+
+    public TakeQuizState(User user, MCQuiz quiz) {
+        this.user = user;
+        this.quiz = quiz;
+    }
+
+    public TakeQuizState() {}
+
+    // Getting functions
+    public User getUser() {
+        return this.user;
+    }
+
+    public MCQuiz getQuiz() {
+        return this.quiz;
+    }
+
+    // Setting functions
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public void setQuiz(MCQuiz quiz) {
+        this.quiz = quiz;
+    }
 }
