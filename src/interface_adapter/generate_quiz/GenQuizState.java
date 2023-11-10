2242b6f833d1455ac02ba3c5cde2917d296b3862
mark <mark.henein9616@gmail.com>
2023-11-10 14:49:30 -0500

    Implemented the boilerplate for the use case states

--- src/interface_adapter/generate_quiz/GenQuizState.java
+++ src/interface_adapter/generate_quiz/GenQuizState.java
@@ -1,4 +1,45 @@
 package interface_adapter.generate_quiz;
 
+import entity.DifficultyLevel;
+import entity.language.Language;
+import entity.reading.Reading;
+
 public class GenQuizState {
+    private Language language;
+    private Reading reading;
+    private DifficultyLevel difficultyLevel;
+
+    public GenQuizState(Language language, Reading reading, DifficultyLevel difficultyLevel) {
+        this.language = language;
+        this.reading = reading;
+        this.difficultyLevel = difficultyLevel;
+    }
+
+    public GenQuizState() {}
+
+    // Getting functions
+    public Language getLanguage() {
+        return this.language;
+    }
+
+    public Reading getReading() {
+        return this.reading;
+    }
+
+    public DifficultyLevel getDifficultyLevel() {
+        return this.difficultyLevel;
+    }
+
+    // Setting functions
+    public void setLanguage(Language language) {
+        this.language = language;
+    }
+
+    public void setReading(Reading reading) {
+        this.reading = reading;
+    }
+
+    public void setDifficultyLevel(DifficultyLevel difficultyLevel) {
+        this.difficultyLevel = difficultyLevel;
+    }
 }
