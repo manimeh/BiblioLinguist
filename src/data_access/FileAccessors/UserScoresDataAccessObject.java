824d00196a54f088cc054071c35949da5fa6d5fa
Franco Valencia <francomiguel.valencia@gmail.com>
2023-11-13 21:02:36 -0500

    Unimplemented methods for UserScoresDataAccessObject

--- src/data_access/FileAccessors/UserScoresDataAccessObject.java
+++ src/data_access/FileAccessors/UserScoresDataAccessObject.java
@@ -1,4 +1,20 @@
 package data_access.FileAccessors;
+import com.opencsv.CSVReader;
+import com.opencsv.CSVReader;
+import com.opencsv.CSVWriter;
+
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class UserScoresDataAccessObject {
+    public  void addToQueue(float number) {
+
+    }
+
+    public ArrayList getLastTenScores() {
+
+    }
 }
