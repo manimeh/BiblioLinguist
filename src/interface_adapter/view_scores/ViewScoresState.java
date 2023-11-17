680314f09df6ae1365e561e89aa9e666bc03994b
Mani Mehdipoor <mani.mehdipoor@mail.utoronto.ca>
2023-11-17 17:31:49 -0500

    added a few suggestions to Franco2 branch

--- src/interface_adapter/view_scores/ViewScoresState.java
+++ src/interface_adapter/view_scores/ViewScoresState.java
@@ -3,6 +3,15 @@ package interface_adapter.view_scores;
 public class ViewScoresState {
     private String viewScoresMessage = null;
 
+    //A copy constructor is needed for states to construct a state based on a copy
+    public ViewScoresState(ViewScoresState copy) {
+        viewScoresMessage = copy.viewScoresMessage;
+    }
+
+    // Because of the previous copy constructor, the default constructor must be explicit.
+    public ViewScoresState() {
+    }
+
     public void setViewScoresMessage(String s) {
         this.viewScoresMessage = s;
     }
