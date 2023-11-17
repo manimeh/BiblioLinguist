0dd26523572e8a68f6bc50b9e9483358cbc9624d
Franco Valencia <francomiguel.valencia@gmail.com>
2023-11-16 23:15:19 -0500

    Added ViewManagerModel in ViewScoresPresenter as a variable

--- src/interface_adapter/view_scores/ViewScoresPresenter.java
+++ src/interface_adapter/view_scores/ViewScoresPresenter.java
@@ -1,13 +1,16 @@
 package interface_adapter.view_scores;
 
+import interface_adapter.ViewManagerModel;
 import use_case.view_scores.ViewScoresOutputBoundary;
 import use_case.view_scores.ViewScoresOutputData;
 
 public class ViewScoresPresenter implements ViewScoresOutputBoundary {
     private final ViewScoresViewModel viewScoresViewModel;
+    private final ViewManagerModel viewManagerModel;
 
-    public ViewScoresPresenter(ViewScoresViewModel viewScoresViewModel) {
+    public ViewScoresPresenter(ViewManagerModel viewManagerModel, ViewScoresViewModel viewScoresViewModel) {
         this.viewScoresViewModel = viewScoresViewModel;
+        this.viewManagerModel = viewManagerModel;
     }
 
     @Override
@@ -21,7 +24,7 @@ public class ViewScoresPresenter implements ViewScoresOutputBoundary {
                 sumOfScores += i;
             }
             ViewScoresState viewScoresState = viewScoresViewModel.getState();
-            viewScoresState.setViewScoresMessage("Your last 10 scores: " + String.join(",", response.getScoresArray().toString().substring(1, )) + "\nAverage from your last 10 scores: ");
+            viewScoresState.setViewScoresMessage("Your last 10 scores: " + String.join(",", response.getScoresArray().toString().substring(1, response.getScoresArray().size() - 1)) + "\nAverage from your last 10 scores: ");
 
         }
     }
