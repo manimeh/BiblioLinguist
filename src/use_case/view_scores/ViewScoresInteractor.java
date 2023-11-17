c884f7e2b55acd0167f9589ef86fd241bbe1ae82
Franco Valencia <francomiguel.valencia@gmail.com>
2023-11-16 22:26:57 -0500

    Added an instantiation method and execute method

--- src/use_case/view_scores/ViewScoresInteractor.java
+++ src/use_case/view_scores/ViewScoresInteractor.java
@@ -1,4 +1,18 @@
 package use_case.view_scores;
 
-public class ViewScoresInteractor {
+public class ViewScoresInteractor implements ViewScoresInputBoundary{
+    final ViewScoresDataAccessInterface viewScoresDataAccessObject;
+    final ViewScoresOutputBoundary viewScoresPresenter;
+
+    public ViewScoresInteractor(ViewScoresDataAccessInterface viewScoresDataAccessObject, ViewScoresOutputBoundary viewScoresPresenter) {
+        this.viewScoresDataAccessObject = viewScoresDataAccessObject;
+        this.viewScoresPresenter = viewScoresPresenter;
+    }
+
+
+    @Override
+    public void execute() {
+        ViewScoresOutputData viewScoresOutputData = new ViewScoresOutputData(viewScoresDataAccessObject.getLastTenScores());
+        viewScoresPresenter.prepareScoreView(viewScoresOutputData);
+    }
 }
