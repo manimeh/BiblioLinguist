8df476361554be2d3880d3e95c035501bcb277a7
Mani Mehdipoor <mani.mehdipoor@mail.utoronto.ca>
2023-11-17 23:40:49 -0500

    Made a few suggestions to Mark's branch

--- src/entity/DifficultyLevel.java
+++ src/entity/DifficultyLevel.java
@@ -2,19 +2,25 @@ package entity;
 
 public enum DifficultyLevel
 {
-    BEGINNER("Beginner", 55, 100),
-    INTERMEDIATE("Intermediate", 40, 55),
-    ADVANCED("Advanced", 10, 40);
+    BEGINNER(0, "Beginner", 55, 100),
+    INTERMEDIATE(1, "Intermediate", 40, 55),
+    ADVANCED(2, "Advanced", 10, 40);
 
+    private final int index;
     private final String name;
     private final int minReadRange;
     private final int maxReadRange;
 
-    DifficultyLevel(String name, int minReadRange, int maxReadRange)
+    DifficultyLevel(int index, String name, int minReadRange, int maxReadRange)
     {
+        this.index = index;
         this.name = name;
         this.minReadRange = minReadRange;
         this.maxReadRange = maxReadRange;
+    }
+
+    public int getIndex() {
+        return index;
     }
 
     public String getName() {
