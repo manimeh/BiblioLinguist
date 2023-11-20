ab0e16243188937c981e9c2ba62048e28f81ede8
Mani Mehdipoor <mani.mehdipoor@mail.utoronto.ca>
2023-11-20 12:03:44 -0500

    added UserFactory

--- src/entity/user/UserFactory.java
+++ src/entity/user/UserFactory.java
@@ -1,2 +1,9 @@
-package entity.user;public class UserFactory {
+package entity.user;
+
+public class UserFactory {
+    private static int id;
+
+    public User createUser(String name, float[][] scores) {
+        return new User(id++, name, scores);
+    }
 }
