76b0a7e6d62f29a4ccc6f019526cac24d1b0e06b
Mani Mehdipoor <mani.mehdipoor@mail.utoronto.ca>
2023-11-28 00:12:03 -0500

    added a two test classes. One to check that the code adheres to CA, and one for StartNewGame use case

---
+++ test/use_case/start_new_game/StartNewGameInteractorTest.java
@@ -0,0 +1,27 @@
+package use_case.start_new_game;
+
+import entity.DifficultyLevel;
+import entity.language.Language;
+import entity.reading.ReadingType;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class StartNewGameInteractorTest {
+    @Test
+    void successTest() {
+        StartNewGameDataAccessInterface dataAccessor = new InMemoryUserPreferenceDataAccessObject(Language.ENGLISH,
+                DifficultyLevel.INTERMEDIATE, ReadingType.NEWS);
+
+        // This creates a successPresenter that tests whether the test case is as we expect.
+        StartNewGameOutputBoundary successPresenter = outputData -> {
+            assertEquals(Language.ENGLISH, outputData.defaultLanguage());
+            assertEquals(DifficultyLevel.INTERMEDIATE, outputData.defaultDifficultyLevel());
+            assertEquals(ReadingType.NEWS, outputData.defaultReadingType());
+        };
+
+        StartNewGameInputBoundary interactor = new StartNewGameInteractor(dataAccessor, successPresenter);
+        interactor.execute();
+    }
+}
