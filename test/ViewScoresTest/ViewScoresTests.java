b0d263a49e7259f610ca05215b017c732f15b6e2
Franco Valencia <fma.valencia@icloud.com>
2023-11-27 22:18:43 -0500

    Implemented methods for adding 1 score as well as 10 scores to the UserScores csv file.
    Wrote a test that makes sure that the saveScore properly adds consecutive scores to the csv file, and that the earliest score is erased.
    
    Furthermore, this test ensures that getLastTenScores properly returns the scores in the correct format (ArrayList<Float>)

--- test/ViewScoresTest/ViewScoresTests.java
+++ test/ViewScoresTest/ViewScoresTests.java
@@ -2,7 +2,13 @@ package ViewScoresTest;
 
 import app.Main;
 import data_access.file_accessors.UserScoresDataAccessObject;
+import entity.DifficultyLevel;
 import entity.user.User;
+import interface_adapter.view_scores.ViewScoresController;
+import org.junit.Assert;
+import use_case.view_scores.ViewScoresInputBoundary;
+import use_case.view_scores.ViewScoresInteractor;
+import view.HomePageView;
 
 import javax.swing.*;
 import java.awt.*;
@@ -12,6 +18,9 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.stream.Collectors;
 
 import static org.junit.Assert.*;
 
@@ -22,28 +31,54 @@ public class ViewScoresTests {
     /**
      * adds ten consecutive scores to the UserScores CSV file.
      */
-    public void addTenScores() {
-        UserScoresDataAccessObject userScoresDataAccessObject;
-        User user;
-        try {
-            userScoresDataAccessObject = new UserScoresDataAccessObject("./users.csv");
-            user = new User(1, "Billy", new float[0][]);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
+    public void addTenDifferentScores(UserScoresDataAccessObject userData, User user) {
+        userData.saveScore(10.0f, user);
+        userData.saveScore(20.0f, user);
+        userData.saveScore(30.0f, user);
+        userData.saveScore(40.0f, user);
+        userData.saveScore(50.0f, user);
+        userData.saveScore(60.0f, user);
+        userData.saveScore(70.0f, user);
+        userData.saveScore(10.0f, user);
+        userData.saveScore(20.0f, user);
+        userData.saveScore(30.0f, user);
+    }
+
+    public void addOneMoreScore(UserScoresDataAccessObject userData, User user) {
+        userData.saveScore(15.0f, user);
+    }
+
+    public JButton getButton() {
+        JFrame app = null;
+        Window[] windows = Window.getWindows();
+        for (Window window : windows) {
+            if (window instanceof JFrame) {
+                app = (JFrame) window;
+            }
         }
-        userScoresDataAccessObject.saveScore(10.0f, user);
-        userScoresDataAccessObject.saveScore(20.0f, user);
-        userScoresDataAccessObject.saveScore(30.0f, user);
-        userScoresDataAccessObject.saveScore(40.0f, user);
-        userScoresDataAccessObject.saveScore(50.0f, user);
-        userScoresDataAccessObject.saveScore(60.0f, user);
-        userScoresDataAccessObject.saveScore(70.0f, user);
-        userScoresDataAccessObject.saveScore(10.0f, user);
-        userScoresDataAccessObject.saveScore(20.0f, user);
-        userScoresDataAccessObject.saveScore(30.0f, user);
+        assertNotNull(app); // found the window?
+        Component root = app.getComponent(0);
+        Component cp = ((JRootPane) root).getContentPane();
+        JPanel jp = (JPanel) cp;
+        JPanel jp2 = (JPanel) jp.getComponent(0);
+        HomePageView sv = (HomePageView) jp2.getComponent(0);
+        JPanel buttons = (JPanel) sv.getComponent(4);
+        return (JButton) buttons.getComponent(2); // this should be the clear button
     }
 
-    public void addEleventhScore() {
-
+    /**
+     * Test that the earliest score is replaced when an eleventh score is added
+     * @throws IOException
+     */
+    @org.junit.Test
+    public void addEleventhScore() throws IOException {
+        UserScoresDataAccessObject userData = new UserScoresDataAccessObject("./users.csv");
+        User user = new User(1, "Billy", new float[DifficultyLevel.values().length][]);
+        ArrayList<Float> expectedScores = new ArrayList<>(
+                Arrays.asList(20.0f, 30.0f, 40.0f, 50.0f, 60.0f, 70.f, 10.0f, 20.0f, 30.0f, 15.0f));
+        addTenDifferentScores(userData, user);
+        addOneMoreScore(userData, user);
+        Assert.assertEquals(expectedScores, userData.getLastTenScores());
     }
+
 }
